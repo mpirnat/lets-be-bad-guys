0f7853520d024e8624885e2957c440993b6fd4f3
David Stanek <dstanek@dstanek.com>
2013-01-27 17:01:03 -0500

    Initial dump of injection flaws

--- badguys/vulnerable/views.py
+++ badguys/vulnerable/views.py
@@ -1,1 +1,67 @@
-# Create your views here.
+import base64
+import mimetypes
+import os
+import urllib
+
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+
+
+def norm(s):
+    return s.strip().replace(' ', '').lower()
+
+
+def sql(request):
+    expected_sql = ("SELECT id from Users where first_name = ''; "
+                    "DROP TABLE Users;--';")
+    expected_sql = "'; DROP TABLE Users;--"
+
+    name = request.POST['name'] if request.method == 'POST' else ''
+    correct = (norm(name) == norm(expected_sql))
+
+    return render(request, 'vulnerable/injection/sql.html',
+            {'name': name, 'correct': correct})
+
+
+def file_access(request):
+    msg = request.GET.get('msg', '')
+    return render(request, 'vulnerable/injection/file_access.html',
+            {'msg': msg})
+
+
+def user_profile(request):
+    """A view that is vulnerable to malicious file access."""
+
+    base_path = os.path.join(os.path.dirname(__file__), '../../badguys_project/static/images')
+    filename = request.GET.get('p')
+    if filename.startswith('/'):
+        msg = urllib.quote_plus("Did you really think it would be "
+                "that easy? Bonus points if you see the XSS flaw.")
+        location = '/danger/vulnerable/file-access?msg=' + msg
+        return redirect(reverse('injection-file-access') + "?msg=" + msg)
+
+    data = file(os.path.join(base_path, filename)).read()
+    return HttpResponse(data, content_type=mimetypes.guess_type(filename)[0])
+
+
+def code_execution(request):
+    msg = ''
+    first_name = ''
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name', '')
+        exec(base64.decodestring(first_name))
+        try:
+            data = open('/tmp/p0wned.txt').read()
+            msg = ('Good job! You successfully p0wned yourself. You '
+                   'wrote: %s' % data)
+        except IOError:
+            data = ''
+            msg = 'You failed to write a file.'
+
+    return render(request, 'vulnerable/injection/code_execution.html',
+            {'first_name': request.POST.get('first_name', ''), 'msg': msg})
+
+    # maybe put this is some sort of hints
+    # solution: >>> import base64
+    #           >>> base64.encodestring("file('/tmp/p0wned.txt', 'w').write('boom!')")
--- badguys_project/urls.py
+++ badguys_project/urls.py
@@ -1,11 +1,42 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.base import TemplateView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name="home"),
+    url(r'^about$',
+        TemplateView.as_view(template_name='about.html'), name="about"),
+)
+
+urlpatterns += patterns('badguys.vulnerable.views',
+    url(r'^injection$',
+        TemplateView.as_view(template_name='vulnerable/injection/index.html'),
+        name="injection"),
+    url(r'^injection/sql$', 'sql', name="injection-sql"),
+    url(r'^injection/file-access$', 'file_access',
+        name="injection-file-access"),
+    url(r'^user-profile$', 'user_profile', name='user-profile'),
+    url(r'^injection/code-execution$', 'code_execution',
+        name="injection-code-execution"),
+
+    url(r'^cross-site-scripting$',
+        TemplateView.as_view(template_name='vulnerable/xss/index.html'),
+        name="xss"),
+    url(r'^cross-site-scripting/path-matching$',
+        TemplateView.as_view(template_name='vulnerable/xss/path.html'),
+        name="xss-path"),
+    url(r'^cross-site-scripting/form-field$',
+        TemplateView.as_view(template_name='vulnerable/xss/form.html'),
+        name="xss-form"),
+    url(r'^cross-site-scripting/query-params$',
+        TemplateView.as_view(template_name='vulnerable/xss/query.html'),
+        name="xss-query"),
+
     # url(r'^$', 'badguys_project.views.home', name='home'),
     # url(r'^badguys_project/', include('badguys_project.foo.urls')),
 
@@ -15,3 +46,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
