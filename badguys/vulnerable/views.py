538e30a69e0eb5db6c4c4c0d3a16e41c1a908627
Daghan Altas <daghan@gmail.com>
2020-08-19 14:16:44 -0700

    adding a semgrep test file

--- badguys/vulnerable/views.py
+++ badguys/vulnerable/views.py
@@ -10,66 +10,69 @@ from django.views.decorators.csrf import csrf_exempt
 
 ## 01 - Injection Attacks
 
+
 def norm(s):
-    return s.strip().replace(' ', '').lower()
+    return s.strip().replace(" ", "").lower()
 
 
 def sql(request):
-    solution_sql = ("SELECT id from Users where first_name = ''; "
-                    "DROP TABLE Users;--';")
+    solution_sql = (
+        "SELECT id from Users where first_name = ''; " "DROP TABLE Users;--';"
+    )
     expected_sql = "'; DROP TABLE Users;--"
 
-    name = request.POST['name'] if request.method == 'POST' else ''
-    correct = (norm(name) == norm(expected_sql))
+    name = request.POST["name"] if request.method == "POST" else ""
+    correct = norm(name) == norm(expected_sql)
 
-    return render(request, 'vulnerable/injection/sql.html',
-            {'name': name, 'correct': correct, 'solution_sql': solution_sql})
+    return render(
+        request,
+        "vulnerable/injection/sql.html",
+        {"name": name, "correct": correct, "solution_sql": solution_sql},
+    )
 
 
 def file_access(request):
-    msg = request.GET.get('msg', '')
-    return render(request, 'vulnerable/injection/file_access.html',
-            {'msg': msg})
+    msg = request.GET.get("msg", "")
+    return render(request, "vulnerable/injection/file_access.html", {"msg": msg})
 
 
 def user_pic(request):
     """A view that is vulnerable to malicious file access."""
 
-    base_path = os.path.join(os.path.dirname(__file__), '../../badguys/static/images')
-    filename = request.GET.get('p')
+    base_path = os.path.join(os.path.dirname(__file__), "../../badguys/static/images")
+    filename = request.GET.get("p")
 
     try:
-        data = open(os.path.join(base_path, filename), 'rb').read()
+        data = open(os.path.join(base_path, filename), "rb").read()
     except IOError:
-        if filename.startswith('/'):
+        if filename.startswith("/"):
             msg = "That was worth trying, but won't always work."
-        elif filename.startswith('..'):
+        elif filename.startswith(".."):
             msg = "You're on the right track..."
         else:
             msg = "Keep trying..."
-        return render(request, 'vulnerable/injection/file_access.html',
-                {'msg': msg})
+        return render(request, "vulnerable/injection/file_access.html", {"msg": msg})
 
     return HttpResponse(data, content_type=mimetypes.guess_type(filename)[0])
 
 
 def code_execution(request):
-    data = ''
-    msg = ''
-    first_name = ''
-    if request.method == 'POST':
+    data = ""
+    msg = ""
+    first_name = ""
+    if request.method == "POST":
 
         # Clear out a previous success to reset the exercise
         try:
-            os.unlink('p0wned.txt')
+            os.unlink("p0wned.txt")
         except:
             pass
 
-        first_name = request.POST.get('first_name', '')
+        first_name = request.POST.get("first_name", "")
 
         try:
             # Try it the Python 3 way...
-            exec(base64.decodestring(bytes(first_name, 'ascii')))
+            exec(base64.decodestring(bytes(first_name, "ascii")))
         except TypeError:
             # Try it the Python 2 way...
             try:
@@ -81,12 +84,15 @@ def code_execution(request):
 
         # Check to see if the attack was successful
         try:
-            data = open('p0wned.txt').read()
+            data = open("p0wned.txt").read()
         except IOError:
-            data = ''
+            data = ""
 
-    return render(request, 'vulnerable/injection/code_execution.html',
-            {'first_name': request.POST.get('first_name', ''), 'data': data})
+    return render(
+        request,
+        "vulnerable/injection/code_execution.html",
+        {"first_name": request.POST.get("first_name", ""), "data": data},
+    )
 
 
 ## 02 - Broken Authentication & Session Management
@@ -94,77 +100,76 @@ def code_execution(request):
 
 ## 03 - XSS
 
+
 def xss_form(request):
-    env = {'qs': request.GET.get('qs', 'hello')}
-    response = render(request, 'vulnerable/xss/form.html', env)
-    response.set_cookie(key='monster', value='omnomnomnomnom!')
+    env = {"qs": request.GET.get("qs", "hello")}
+    response = render(request, "vulnerable/xss/form.html", env)
+    response.set_cookie(key="monster", value="omnomnomnomnom!")
     return response
 
 
-def xss_path(request, path='default'):
-    env = {'path': path}
-    return render(request, 'vulnerable/xss/path.html', env)
+def xss_path(request, path="default"):
+    env = {"path": path}
+    return render(request, "vulnerable/xss/path.html", env)
 
 
 def xss_query(request):
-    env = {'qs': request.GET.get('qs', 'hello')}
-    return render(request, 'vulnerable/xss/query.html', env)
+    env = {"qs": request.GET.get("qs", "hello")}
+    return render(request, "vulnerable/xss/query.html", env)
 
 
 ## 04 - Insecure Direct Object References
 users = {
-    '1': {
-        'name': 'Foo',
-        'email': 'foo@example.com',
-        'admin': False,
-    },
-    '2': {
-        'name': 'Bar',
-        'email': 'bar@example.com',
-        'admin': True,
-    }
+    "1": {"name": "Foo", "email": "foo@example.com", "admin": False,},
+    "2": {"name": "Bar", "email": "bar@example.com", "admin": True,},
 }
+
 
 def dor_user_profile(request, userid=None):
     env = {}
     user_data = users.get(userid)
 
-    if request.method == 'POST':
-        user_data['name'] = request.POST.get('name') or user_data['name']
-        user_data['email'] = request.POST.get('email') or user_data['email']
-        env['updated'] = True
+    if request.method == "POST":
+        user_data["name"] = request.POST.get("name") or user_data["name"]
+        user_data["email"] = request.POST.get("email") or user_data["email"]
+        env["updated"] = True
 
-    env['user_data'] = user_data
-    env['user_id'] = userid
-    return render(request, 'vulnerable/direct_object_references/profile.html', env)
+    env["user_data"] = user_data
+    env["user_id"] = userid
+    return render(request, "vulnerable/direct_object_references/profile.html", env)
+
 
 ## 05 - Security Misconfiguration
 
+
 def boom(request):
-    raise Exception('boom')
+    raise Exception("boom")
 
 
 ## 06 - Sensitive Data Exposure
 
+
 def exposure_login(request):
-    return redirect('exposure')
+    return redirect("exposure")
 
 
 ## 07 - Missing Function Level Access Control
 
+
 def missing_access_control(request):
     env = {}
-    if request.GET.get('action') == 'admin':
-        return render(request, 'vulnerable/access_control/admin.html', env)
-    return render(request, 'vulnerable/access_control/non_admin.html', env)
+    if request.GET.get("action") == "admin":
+        return render(request, "vulnerable/access_control/admin.html", env)
+    return render(request, "vulnerable/access_control/non_admin.html", env)
 
 
 ## 08 - CSRF
 
+
 @csrf_exempt
 def csrf_image(request):
-    env = {'qs': request.GET.get('qs', '')}
-    return render(request, 'vulnerable/csrf/image.html', env)
+    env = {"qs": request.GET.get("qs", "")}
+    return render(request, "vulnerable/csrf/image.html", env)
 
 
 ## 09 - Using Known Vulnerable Components
@@ -173,22 +178,23 @@ def csrf_image(request):
 
 ## 10 - Unvalidated Redirects & Forwards
 
+
 def unvalidated_redirect(request):
-    url = request.GET.get('url')
+    url = request.GET.get("url")
     return redirect(url)
 
 
 def unvalidated_forward(request):
-    forward = request.GET.get('fwd')
+    forward = request.GET.get("fwd")
     function = globals().get(forward)
 
     if function:
         return function(request)
 
-    env = {'fwd': forward}
-    return render(request, 'vulnerable/redirects/forward_failed.html', env)
+    env = {"fwd": forward}
+    return render(request, "vulnerable/redirects/forward_failed.html", env)
+
 
 def admin(request):
-    return render(request, 'vulnerable/redirects/admin.html', {})
-
+    return render(request, "vulnerable/redirects/admin.html", {})
 
---
+++ semgrep_test.py
@@ -0,0 +1,4 @@
+foo = ["alice" "bob" "charlie"]
+
+if "alice" is "bob":
+    print("this is a mistake")
